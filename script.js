1f0b544da90976940bbbb3e0828b9280d5703cf2
Shaman03 <shamanelobo56@gmail.com>
2024-12-06 22:45:55 +0000

    shaman

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function signIn() {
-    alert('Sign in clicked!');
+    // Simulate a sign-in process or redirect
+    const userConfirmed = confirm("Would you like to proceed to the sign-in page?");
+    if (userConfirmed) {
+        // Example: Redirecting to a login page
+        window.location.href = "login.html";
+    } else {
+        alert("Sign-in canceled.");
+    }
 }
