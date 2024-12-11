4c3c379c6ae2819b44abfc949f5b046887ee2bde
Shaman03 <shamanelobo56@gmail.com>
2024-12-11 02:46:10 +0000

    add another database

--- process-form.php
+++ process-form.php
@@ -56,4 +56,36 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 // Close the database connection
 $conn->close();
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "`user register`"; // Make sure to use backticks for database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Assuming form data is sent via POST
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];  // You should hash the password in production
+
+$sql = "INSERT INTO `users` (username, email, password) VALUES ('$username', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
 ?>
