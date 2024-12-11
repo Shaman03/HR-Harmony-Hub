4b083167c6ab893b1e50e527a02d0f525026bdf7
Shaman03 <shamanelobo56@gmail.com>
2024-12-11 03:08:42 +0000

    shaman

--- process-form.php
+++ process-form.php
@@ -58,37 +58,3 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 $conn->close();
 
 ?>
-
-
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "`user register`"; //
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Assuming form data is sent via POST
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];  // You should hash the password in production
-
-$sql = "INSERT INTO `users` (username, email, password) VALUES ('$username', '$email', '$password')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-
-
-?>
